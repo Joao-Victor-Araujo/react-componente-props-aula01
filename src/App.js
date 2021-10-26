deeb411953aa732fb055b89a477e0b7bb38e714a
unknown <jvas1999@gmail.com>
2021-10-26 18:55:20 -0300

    up das atv

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Carta} from './Carta';
+
+const App = () =>{
+  return(
+  <div classsName="App">
+<Carta remetente= "João" destino = "pra alguem"/>
+</div>
+);  
+}
+export default App;
+
---
+++ src/Carta.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Carta = (props) => (
+    <div>
+        <h1>Olá, {props.remetente} - {props.destino}</h1>
+        <pre>Estou escrevendo essa carta por causa da atividade que os alunos vão fazer, okay.
+        Não desiste, continue fazendo as atividades, treinando e praticando, O céu é o limite!
+        </pre>
+    </div>
+);
+
+
